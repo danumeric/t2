05b22cffe55886050ce71281999a86b2e708d727
Danila Gridiaev <danila.g@nsysgroup.com>
2023-06-22 18:27:17 +0300

    t4

--- src/components/cards/CardsGallery.vue
+++ src/components/cards/CardsGallery.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="wrapper">
-    <div id="demo__button">Chat</div>
+    <div id="demo__button">
+      <p v-for="n in 100" :key="n">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      </p>
+    </div>
   </div>
 </template>
 
@@ -32,6 +40,6 @@ export default {
   height: 400px;
 }
 #hde-container {
-  inset: 44px 0px 0px 44px;
+  width: 650px;
 }
 </style>
